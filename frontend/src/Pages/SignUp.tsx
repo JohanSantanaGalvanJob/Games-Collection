ca508a58a8c5d30c7b2b57816bcede8a511581c4
Johan Santana Galvan <johanjaviersantanagalvan@alumno.ieselrincon.es>
2023-11-09 20:07:42 +0000

    Done Profile Picture for the Sign Up page

--- frontend/src/Pages/SignUp.tsx
+++ frontend/src/Pages/SignUp.tsx
@@ -1,6 +1,6 @@
 import '../Styles/SignUp.scss'
 
-import { faGamepad } from "@fortawesome/free-solid-svg-icons";
+import { faGamepad, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -18,21 +18,48 @@ function SignUp() {
         navigate('/Home');
     };
 
+    const [previewSrc, setPreviewSrc] = useState('https://cdn-icons-png.flaticon.com/512/4086/4086679.png');
+
+    const handleFileChange = (e: any) => {
+        const file = e.target.files[0];
+
+        if (file) {
+            const reader = new FileReader();
+
+            reader.onload = (readerEvent: any) => {
+                setPreviewSrc(readerEvent.target.result);
+            };
+
+            reader.readAsDataURL(file);
+        } else {
+            setPreviewSrc('default-image.jpg');
+        }
+    };
+
     return (
         <>
             <div className="signup-container">
-                <FontAwesomeIcon icon={faGamepad} className='signup-icon' />
                 <h2 className='signup-title'>Sign Up</h2>
                 <form className="signup-form" onSubmit={handleSubmit}>
-                <label htmlFor="username">Username</label>
+                    <label htmlFor="profilepicture">Profile Picture</label>
+                    <div className='profile-picture-container'>
+                        <div className='profile-picture-image'>
+                            <img src={previewSrc} alt="Imagen" />
+                        </div>
+                        <div>
+                            <input type="file" id="fileInput" accept="image/*" onChange={handleFileChange} style={{ display: 'none' }} />
+                            <FontAwesomeIcon icon={faPlus} onClick={() => { document.getElementById('fileInput')!.click(); }} className='pfp-icon'></FontAwesomeIcon>
+                        </div>
+                    </div>
+                    <label htmlFor="username">Username</label>
                     <input type="text" id="username" name="username" placeholder="helloworld123" value={username}
                         onChange={(e) => setUsername(e.target.value)} required />
                     <label htmlFor="email">Email</label>
                     <input type="email" id="email" name="email" placeholder="hello@example.com" value={email}
-                    onChange={(e) => setEmail(e.target.value)} required/>
+                        onChange={(e) => setEmail(e.target.value)} required />
                     <label htmlFor="password">Password</label>
                     <input type="password" id="password" name="password" placeholder="+8 characters & numbers" value={password}
-                        onChange={(e) => setPassword(e.target.value)} required/>
+                        onChange={(e) => setPassword(e.target.value)} required />
                     <label htmlFor="birthdate">Birth Date</label>
                     <input type="date" id="birthdate" name="birthdate" placeholder="19/11/2003" value={birthdate}
                         onChange={(e) => setBirthdate(e.target.value)} required />
